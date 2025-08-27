1fa4459184a38014c7d382df6ed9e1cff733f7de
maro911220 <kingdomboze@gmail.com>
2025-08-27 18:54:38 +0900

    langauge korea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ko" suppressHydrationWarning>
       <head />
       <body>
         <ThemeProvider
